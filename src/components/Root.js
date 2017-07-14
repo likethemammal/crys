ef575c29f6ad19258ac56ae4161fddcc2f563dd0
Chris Dolphin <cdolphin1991@gmail.com>
2017-07-13 20:25:13 -0400

    Update store configuration and store naming logic

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react'
 import { Provider } from "react-redux"
-import configureStore from "../store/configure-store"
+import { appStore } from "../store"
 import App from '../containers/App'
-
-const store = configureStore();
 
 class Root extends Component {
     render() {
-        return <Provider store={store}>
+        return <Provider store={appStore}>
             <App />
         </Provider>
     }
